06f5ed668ac44cb99f4ef44b039e31f4e89df901
dbturner <turne400@msu.edu>
2020-08-07 17:56:34 -0400

    major changes to data structure and documentation

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' Curacao arthropods
+#' curacao.arthropods
 #'
 #' Some description.
 #'
@@ -16,7 +16,137 @@
 #'   \item{Lacewings}{proportion of all arthropods that were lacewings (Neuroptera)}
 #'   \item{Crickets}{Proportion of all arthropods that were crickets (Orthoptera)}
 
-#' }
-#' @source \url{https://github.com/dbturner/c4bi/blob/main/data/Curacao%20arthropods.csv}
-"Curacao arthropods"
 
+#' }
+"curacao.arthropods"
+
+
+#' curacao.exo.birds.sum
+#'
+#' Some description.
+#'
+#' @format
+#' \describe{
+#'   \item{species}{Common name of exotic bird species observed on properties}
+#'   \item{Site Type}{Land-use category for the focal property}
+#'   \item{Percentage of Sites}{The number of sites where each bird species was observed in each category}
+
+#' }
+"curacao.exo.birds.sum"
+
+
+#' curacao.exo.birds
+#'
+#' Some description.
+#'
+#' @format
+#' \describe{
+#'   \item{Site}{name of the focal property (anonymized by letter here)}
+#'   \item{sitetype}{land-use category for the focal property}
+#'   \item{Exotic parrots}{abundance of exotic parrots at focal property}
+#'   \item{Chestnut-fronted Macaw}{abundance of Chestnut-fronted Macaws at focal property}
+#'   \item{Rock Pigeon}{abundance of Rock Pigeons at focal property}
+#'   \item{House Sparrow}{abundance of House Sparrows at focal property}
+#'   \item{Saffron Finch}{abundance of Saffron Finches at focal property}
+#'   \item{ReportName}{name of report for references (not required)}
+#'
+#' }
+"curacao.exo.birds"
+
+
+#' curacao.exo.herps
+#'
+#' Some description.
+#'
+#' @format
+#' \describe{
+#'   \item{Site}{name of the focal property (anonymized by letter here)}
+#'   \item{sitetype}{land-use category for the focal property}
+#'   \item{Mourning Gecko}{Abundance of Mourning Gecko at focal property.}
+#'   \item{Tropical House Gecko}{Abundance of Tropical House Gecko at focal property.}
+#'   \item{Johnstone's Whistling Frog}{Abundance of Johnstone's Whistling Frog at focal property}
+#'   \item{Cuban Tree Frog}{Abundance of Cuban Tree Frog at focal property}
+#'   \item{Colombian Four-eyed Frog}{Abundance of Colombian Four-eyed Frog at focal property}
+#'   \item{Asian House Gecko}{Abundance of Asian House Gecko at focal property}
+#'   \item{ReportName}{name of report for references (not required)}
+#'
+#' }
+"curacao.exo.herps"
+
+
+#' curacao.exo.herps.sum
+#'
+#' Some description.
+#'
+#' @format
+#' \describe{
+#'   \item{species}{Common name of exotic herp species observed on properties}
+#'   \item{Site Type}{Land-use category for the focal property}
+#'   \item{Percentage of Sites}{The number of sites where each herp species was observed in each category}
+#'
+#' }
+"curacao.exo.herps.sum"
+
+
+#' curacao.nat.bird.list
+#'
+#' Some description.
+#'
+#' @format
+#' \describe{
+#'   \item{Site}{Name of the focal property (anonymized by letter here)}
+#'   \item{sitetype}{Land-use category for the focal property}
+#'   \item{Scientific name}{The number of sites where each herp species was observed in each category}
+#'   \item{Present}{The number of sites where each herp species was observed in each category}
+#'   \item{Common Name}{Common name of native bird species in Curaçao}
+#'
+#' }
+"curacao.nat.birds.list"
+
+
+#' curacao.nat.bird
+#'
+#' Some description.
+#'
+#' @format
+#' \describe{
+#'   \item{Site}{Name of the focal property (anonymized by letter here)}
+#'   \item{sitetype}{Land-use category for the focal property}
+#'   \item{SR}{Species richness of native birds found at the focal property}
+#'   \item{ReportName}{Name of report for references (not necessary)}
+#'
+#' }
+"curacao.nat.birds"
+
+
+#' curacao.nat.herp.sum
+#'
+#' Some description.
+#'
+#' @format
+#' \describe{
+#'   \item{sitetype}{Land-use category for the focal property}
+#'   \item{species}{Common name of native herp species on properties}
+#'   \item{mean}{Mean abundance of each native herp species on each property within a land-use category}
+#'
+#' }
+"curacao.nat.herps.sum"
+
+
+#' curacao.nat.herps
+#'
+#' Some description.
+#'
+#' @format
+#' \describe{
+#'   \item{Site}{name of the focal propety (anonymized by letter here)}
+#'   \item{sitetype}{land-use category for the focal property}
+#'   \item{Dutch Leaf-toed Gecko}{abundance of Dutch Leaf-toed Gecko at focal property}
+#'   \item{Antillean Dwarf Gecko}{abundance of Antillean Dwarf Gecko at focal property}
+#'   \item{Striped Anole}{abundance of Striped Anole at focal property}
+#'   \item{Laurent's Whiptail}{abundance of Laurent's Whiptail at focal property}
+#'   \item{Turnip-tailed Gecko}{abundance of Turnip-tailed Gecko at focal property}
+#'   \item{ReportName}{name of report for references (not necessary)}
+#'
+#' }
+"curacao.nat.herps"
